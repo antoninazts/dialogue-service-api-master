115581cbd5f19779732115c9f13b9b76cf78cd77
antoninazts <m2314595@edu.misis.ru>
2024-04-10 18:20:57 +0300

    initial commit

---
+++ app/services/openai_svc.py
@@ -0,0 +1,32 @@
+import requests
+from app.core.config import settings
+import time
+
+
+def generate_openai_prompt(prompt: str) -> str:
+    """
+    Simple client for OpenAi Api request
+    """
+    # massages = [
+    #     {
+    #         'role': 'user',
+    #         'content': prompt,
+    #     }
+    # ]
+    # with requests.Session() as session:
+    #     response = session.post(
+    #         url=settings.OPENAI_URL,
+    #         json={
+    #             'model': 'gpt-4',
+    #             'messages': massages,
+    #         },
+    #         headers={'Authorization': f'Bearer {settings.OPENAI_TOKEN}'},
+    #         timeout=15 * 60,
+    #     )
+    #     result = response.json()
+
+    # return result['choices'][0]['message']['content']
+
+    time.sleep(10)
+    return "test_77"
+
